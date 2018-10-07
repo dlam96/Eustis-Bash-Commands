80fd5f973762fa83d1f200499c0cd65fd246cbe3
dlam96 <dannyfion@hotmail.com>
2018-10-07 17:23:55 -0400

    updated autolog

--- auto-login.sh
+++ auto-login.sh
@@ -1,1 +1,1 @@
-SSHPASS='Iloveunicorns1@' sshpass -e ssh -o StrictHostKeyChecking=no da591064@eustis.eecs.ucf.edu
+SSHPASS='password' sshpass -e ssh -o StrictHostKeyChecking=no username@eustis.eecs.ucf.edu
